0a361061a6105a7a19cc3be4365386900676652b
Froilan Irizarry <froilan@froilanirizarry.me>
2018-01-30 17:52:15 -0500

    Add index changes. - Added compliance dashboard conditionals.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request-promise')
 const errors = require('request-promise/errors')
 const fs = require('fs')
 const lunr = require('lunr')
-const { validateJson, upgradeProject, getAgencyStatus, handleError } = require('./libs/utils')
+const { validateJson, upgradeProject, getAgencyStatus, handleError, mergeJson } = require('./libs/utils')
 const Reporter  = require('./libs/reporter')
 const JsonFile = require('jsonfile')
 
@@ -36,7 +36,10 @@ function validate(data, agency, reporter) {
   const results = validateJson(data)
 
   if (results.errors) {
-    reporter.reportIssues(data.agency, results)
+    if(agency.complianceDashboard) {
+      reporter.reportIssues(data.agency, results)
+    }
+    
     return false
   }
 
@@ -56,6 +59,10 @@ function format(data) {
   }
 }
 function writeReleasesFiles(releasesJson) {
+  const defaultReleases = JsonFile.readFileSync('./data/defaultReleases.json')
+
+  releasesJson = mergeJson(releasesJson, defaultReleases.releases)
+
   const releasesString = JSON.stringify({
     releases: releasesJson
   })
@@ -94,9 +101,10 @@ function getCodeJson(requestOptions, agencyMetadata, reporter) {
         throw `Error formatting code.json for Agency: ${agencyMetadata.acronym} - `, err
       }
       
-      reporter.reportVersion(formattedData.agency, formattedData.version)
-      reporter.reportMetadata(formattedData.agency, agencyMetadata)
-
+      if(agencyMetadata.complianceDashboard) {
+        reporter.reportVersion(formattedData.agency, formattedData.version)
+        reporter.reportMetadata(formattedData.agency, agencyMetadata)
+      }
       const isCodeJsonValid = validate(formattedData, agencyMetadata, reporter)
 
       if (!isCodeJsonValid) {
@@ -119,10 +127,12 @@ function getCodeJson(requestOptions, agencyMetadata, reporter) {
       return { releases: [] }
     })
     .finally(() => {
-      const agencyStatus = getAgencyStatus(agencyMetadata)
 
-      reporter.reportStatus(agencyMetadata.acronym, agencyStatus)
-      reporter.reportRequirements(agencyMetadata.acronym, agencyMetadata.requirements)
+      if(agencyMetadata.complianceDashboard) {
+        const agencyStatus = getAgencyStatus(agencyMetadata)
+        reporter.reportStatus(agencyMetadata.acronym, agencyStatus)
+        reporter.reportRequirements(agencyMetadata.acronym, agencyMetadata.requirements)
+      }
     })
 }
 
