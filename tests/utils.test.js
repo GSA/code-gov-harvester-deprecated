1420460874965f77ae2ae52bd72d811d9e6e273b
Froilan Irizarry <froilan@froilanirizarry.me>
2018-04-09 14:51:58 -0400

    Fix eslint warnings and errors. - Changed double quotes to single quotes - Formated document spacing

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,62 +1,61 @@
-const Ajv = require('ajv')
+// const Ajv = require('ajv')
 const chai = require('chai'),
-      should = chai.should(),
-      expect = chai.expect
+  should = chai.should(),
+  expect = chai.expect
 
 const validateJson = require('../libs/utils').validateJson,
-    upgradeProject = require('../libs/utils').upgradeProject,
-    mergeJson = require('../libs/utils').mergeJson
+  upgradeProject = require('../libs/utils').upgradeProject,
+  mergeJson = require('../libs/utils').mergeJson
 
-describe('Json Validation', function() {
-    it('wrong data for schema v1.0.1', function(){
-        const badJson = require('./bad-data.json')
-        const validator = validateJson(badJson)
+describe('Json Validation', function () {
+  it('wrong data for schema v1.0.1', function () {
+    const badJson = require('./test-data/bad-data.json')
+    const validator = validateJson(badJson)
 
-        validator.should.have.property('errors').not.equal(null)
-    })
-    it('correct schema v1.0.1', function() {
-        const goodJson = require('./good-data.json')
-        const validator = validateJson(goodJson)
+    validator.should.have.property('errors').not.equal(null)
+  })
+  it('correct schema v1.0.1', function () {
+    const goodJson = require('./test-data/good-data.json')
+    const validator = validateJson(goodJson)
 
-        validator.should.have.property('errors').equal(null)
-    })
+    validator.should.have.property('errors').equal(null)
+  })
 })
 
-describe('Upgrade Code.json to 2.0.0', function() {
-    it('upgrades code.json from 1.0.1 to 2.0.0', function() {
-        const codeJson = require('./code.json')
-        const expectedReleasesLength = codeJson.projects.length
+describe('Upgrade Code.json to 2.0.0', function () {
+  it('upgrades code.json from 1.0.1 to 2.0.0', function () {
+    const codeJson = require('./test-data/code.json')
+    // const expectedReleasesLength = codeJson.projects.length
 
-        codeJson.releases = codeJson.projects.map(upgradeProject)
+    codeJson.releases = codeJson.projects.map(upgradeProject)
 
-        codeJson.releases[0].should.have.property('relatedCode')
-    })
+    codeJson.releases[0].should.have.property('relatedCode')
+  })
 })
 
-describe('Merge releases', function() {
-    it('merge two releases json objects', function() {
-        const expectedJson = {
-            "releases": {
-                "name": "Joe",
-                "lastName": "Smith",
-                "age": 36
-            }
-        }
-        const mergeToJson = {
-            "releases": {
-                "name": "Joe",
-                "lastName": "Smith",
-            }
-        }
-        const mergeFromJson = 
-        {
-            "releases": {
-                "age": 36
-            }
-        }
+describe('Merge releases', function () {
+  it('merge two releases json objects', function () {
+    const expectedJson = {
+      'releases': {
+        'name': 'Joe',
+        'lastName': 'Smith',
+        'age': 36
+      }
+    }
+    const mergeToJson = {
+      'releases': {
+        'name': 'Joe',
+        'lastName': 'Smith',
+      }
+    }
+    const mergeFromJson = {
+      'releases': {
+        'age': 36
+      }
+    }
 
-        releasesResult = mergeJson(mergeToJson.releases, mergeFromJson.releases)
+    const releasesResult = mergeJson(mergeToJson.releases, mergeFromJson.releases)
 
-        expect(releasesResult).to.deep.equal(expectedJson.releases)
-    })
+    expect(releasesResult).to.deep.equal(expectedJson.releases)
+  })
 })
