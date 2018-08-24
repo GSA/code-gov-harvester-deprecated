619bf53522f007d25bf5216b1d954ff018134dbe
Froilan Irizarry <froilan@froilanirizarry.me>
2018-08-23 20:35:42 -0400

    Refactored code to use async/await

--- libs/index_tools/index_optimizer.js
+++ libs/index_tools/index_optimizer.js
@@ -1,5 +1,5 @@
 const Logger = require('../logger');
-const logger = new Logger({ name: 'inde-optimizer' });
+const logger = new Logger({ name: 'index-optimizer' });
 
 /**
  * Optimizes (forceMerge) an index into 1 segment so that
@@ -9,23 +9,19 @@ const logger = new Logger({ name: 'inde-optimizer' });
  * @param {string} indexName The index to optimize.
  * @returns {Promise} Promise with Elasticsearch result.
  */
-function optimizeIndex(indexName, adapter) {
-  return new Promise((resolve, reject) => {
-    logger.debug(`Optimizing Index (${indexName})`);
-    adapter.client.indices.forcemerge({
+async function optimizeIndex(indexName, adapter) {
+  logger.debug('Entered optimizeIndex');
+
+  try {
+    return await adapter.client.indices.forcemerge({
       maxNumSegments: 1,
       index: indexName,
       requestTimeout: 90000
-    }, (error, response, status) => {
-      if(error) {
-        reject(error);
-      }
-      if (status) {
-        this.logger.debug('Status', status);
-      }
-      resolve(response);
     });
-  });
+  } catch(error) {
+    logger.trace(error);
+    throw error;
+  }
 }
 
 module.exports = optimizeIndex;
